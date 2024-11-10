1ed7415fff784866aeae729f8f0f29c3c67d9367
AjmalKBangash <ak1489007@gmail.com>
2024-11-10 12:17:04 +0430

    added some txt to count component

---
+++ src/Dev.jsx
@@ -0,0 +1,17 @@
+function Dev() {
+  return (
+    <>
+      <br />
+      <hr />
+      <hr />
+      <hr />
+      <div>Development branch</div>
+      <hr />
+      <hr />
+      <hr />
+      <br />
+    </>
+  );
+}
+
+export default Dev;
